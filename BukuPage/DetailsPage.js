ad5f006d900162b46904ca4a04da66f39e7e14fb
EldraPratama <76528642+EldraPratama@users.noreply.github.com>
2022-09-19 14:11:57 +0700

    1 file modified

--- BukuPage/DetailsPage.js
+++ BukuPage/DetailsPage.js
@@ -25,6 +25,10 @@ class DetailsPage extends React.Component {
     return (
       <div className="col-md-12">
       <h2>Input Buku</h2>
+        <Link to="/buku" className="btn btn-info">Kembali</Link>
+        <Link to="/buku" className="btn btn-info mx-5 float-end">Hapus</Link>
+        <Link to="/buku" className="btn btn-info mx-5 float-end">Edit</Link>
+        <div className="mb-4"></div>
       <form name="form" >
 
           <div className={"row form-group"}>
@@ -117,13 +121,6 @@ class DetailsPage extends React.Component {
               </div>  
           </div>
 
-          <div className="form-group">
-              <button className="btn btn-primary">Simpan</button>
-              {/* {adding && 
-                  <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-              } */}
-              <Link to="/buku" className="btn btn-link">Kembali</Link>
-          </div>
       </form>
   </div>
     );
